e375009aa7070b90af72a8a0d535987b2f18db6a
jonnyboi42 <connelljon9@gmail.com>
2024-12-04 15:50:24 -0600

    Added more Selection

--- routes/sabers.js
+++ routes/sabers.js
@@ -31,5 +31,21 @@ export const lightsabers = [
       altText: "Vader Saber",
       initialQuantity: 1,
     },
+    {
+      id: "anakinsaber",
+      name: "Anakin EP3 Saber",
+      price: 497.65,
+      src:"http://localhost:3000/assets/lightsabers/anakinlightsaber.jpg",
+      altText: "Anakin Saber",
+      initialQuantity: 1,
+    },
+    {
+      id: "obiwonsaber",
+      name: "Obi-Won EP3 Saber",
+      price: 497.65,
+      src:"http://localhost:3000/assets/lightsabers/obiwonlightsaber.jpg",
+      altText: "Obi-Won Saber",
+      initialQuantity: 1,
+    },
   ];
   
